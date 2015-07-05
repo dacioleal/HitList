2324ba920a513ef16109869a80280f426fef3ae3
Dacio Jose Leal Rodriguez <dacioleal@MacBook-Pro-de-Dacio.local>
2015-07-05 11:14:09 +0200

    Save in CoreData implementation

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var names = [String]()
+    var people = [NSManagedObject]()
     
     
     @IBAction func addName(sender: AnyObject) {
@@ -21,7 +22,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         
         let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default) { (action: UIAlertAction!) -> Void in
             let textField = alert.textFields![0] as! UITextField
-            self.names.append(textField.text)
+            self.saveName(textField.text)
             self.tableView.reloadData()
         }
         
@@ -50,16 +51,58 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     //MARK: UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return people.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
         
-        cell.textLabel!.text = names[indexPath.row]
+        let person = people[indexPath.row]
+        
+        cell.textLabel!.text = person.valueForKey("name") as! String?
         
         return cell
     }
+    
+    func saveName(name: String) {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext!
+        
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedContext)
+        
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        
+        person.setValue(name, forKey: "name")
+        
+        var error: NSError?
+        
+        if !managedContext.save(&error) {
+            println("Could not save \(error), \(error?.userInfo)")
+        }
+        
+        people.append(person)
+    
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
